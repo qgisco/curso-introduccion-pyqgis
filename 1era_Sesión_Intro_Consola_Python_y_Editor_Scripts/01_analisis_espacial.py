aa7f94e660b5befe7b90d0f0fb78169451391eec
Germán Carrillo <gcarrillo@linuxmail.org>
2020-07-06 08:19:15 -0500

    Add complete script for 1st session

--- 1era_Sesión_Intro_Consola_Python_y_Editor_Scripts/01_analisis_espacial.py
+++ 1era_Sesión_Intro_Consola_Python_y_Editor_Scripts/01_analisis_espacial.py
@@ -1,19 +1,90 @@
-# Cuidado con las 2 siguientes líneas. 
-# En el taller las ejecutamos en consola, 
-# cambiando manualmente la capa activa
-aeropuertos = iface.activeLayer()
-provincias = iface.activeLayer()
+# Obtener capa activa. ¿Capa activa es None?
+layer_a = iface.activeLayer()
+if layer_a is None:
+  print("No hay capa activa")
+else:
+  print("La capa activa es:", layer_a.name())
 
-# La siguiente línea se ejecutó luego de haber 
-# seleccionado el departamento de Santander 
-# manualmente en el mapa
-f_s = provincias.selectedFeatures()[0]
 
-g_s = f_s.geometry()
+# featureCount()
+print("El número de registros en la capa activa es:", layer_a.featureCount())
 
-fs_a = [f for f in aeropuertos.getFeatures()]
 
-for f_a in fs_a: 
-    res = f_a.geometry().intersects(g_s)
-    if res:
-        print(f_a["name"])
+# Número de campos
+print("El número de campos en la capa activa es:", len(layer_a.fields()))
+
+
+# Nombre de todos los campos
+for field in layer_a.fields():
+  print(field.name())
+  
+
+# Acceder a atributos
+feature = layer_a.getFeature(1)
+print(feature["name"])
+
+
+# Nombre de todos los aeropuertos
+for feature in layer_a.getFeatures():
+  print(feature["name"])
+
+
+# Nombre y coords de todos los aeropuertos
+for feature in layer_a.getFeatures():
+  print(feature["name"], "-", feature.geometry().asWkt())
+  
+
+# Selección de departamento de Antioquia por expresión
+layer_d = QgsProject.instance().mapLayersByName("Provinces")[0]
+layer_d.selectByExpression("name = 'Antioquia'")
+
+
+# Leer geometría seleccionada
+dpto = layer_d.selectedFeatures()[0].geometry()
+
+
+# ¿Qué aeropuertos se intersectan con el departamento seleccionado?
+for aeropuerto in layer_a.getFeatures():
+	if aeropuerto.geometry().intersects(dpto):
+		print(aeropuerto['name'])
+
+
+# Listar parejas:  Aeropuerto --> Departamento
+for dpto in layer_d.getFeatures():
+    for aeropuerto in layer_a.getFeatures():
+        if aeropuerto.geometry().intersects(dpto.geometry()):
+            print(aeropuerto['name'], "-->", dpto['name'])
+
+
+# Campo nuevo --> "dpto"
+layer_a.dataProvider().addAttributes([QgsField("dpto", QVariant.String)])
+la.updateFields()
+
+
+# ¿Cómo actualizar el valor de un campo?
+dpto_idx = layer_a.fields().indexOf('dpto')
+
+with edit(layer_a):
+  layer_a.changeAttributeValue(1, dpto_idx, 'Santander')
+
+
+# Llenar en el campo nuevo el nombre del departamento
+with edit(layer_a):
+  for dpto in layer_d.getFeatures():
+    for aeropuerto in layer_a.getFeatures():
+      if aeropuerto.geometry().intersects(dpto.geometry()):
+        layer_a.changeAttributeValue(aeropuerto.id(), dpto_idx, departamento['name'])
+        break  
+
+
+# Otra manera (sin buffer de edición)
+attr_values = {}
+for dpto in layer_d.getFeatures():
+  for aeropuerto in layer_a.getFeatures():
+    if aeropuerto.geometry().intersects(dpto.geometry()):
+      attr_values[aeropuerto.id()] = {dpto_idx: departamento['name']}
+      break
+
+layer_a.dataProvider().changeAttributeValues(attr_values)
+                
+
