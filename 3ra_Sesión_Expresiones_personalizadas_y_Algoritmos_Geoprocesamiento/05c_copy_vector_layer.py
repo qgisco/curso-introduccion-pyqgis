1fa576374df59aa473e41f70e59327fe700b8a21
Germap <gcarrillo@linuxmail.org>
2021-07-22 19:28:31 -0500

    Add support for tables in copy vector layer
    Since the default for 'types' parameter is only geometry layers.

--- 3ra_Sesión_Expresiones_personalizadas_y_Algoritmos_Geoprocesamiento/05c_copy_vector_layer.py
+++ 3ra_Sesión_Expresiones_personalizadas_y_Algoritmos_Geoprocesamiento/05c_copy_vector_layer.py
@@ -1,10 +1,10 @@
-from qgis.core import QgsFeatureSink
+from qgis.core import QgsFeatureSink, QgsProcessing
 from qgis.processing import alg
 
 @alg(name='pruebaCopyLayer', label='Copy vector layer',
      group='examplescripts', group_label='Example scripts')
 # 'INPUT' is the recommended name for the main input parameter
-@alg.input(type=alg.VECTOR_LAYER, name='INPUT', label='Input vector layer')
+@alg.input(type=alg.VECTOR_LAYER, name='INPUT', label='Input vector layer', types=[QgsProcessing.TypeVector])
 # 'OUTPUT' is the recommended name for the main output parameter
 @alg.input(type=alg.SINK, name='OUTPUT', label='Copy of the vector layer')
 def pruebaCopyLayer(instance, parameters, context, feedback, inputs):
